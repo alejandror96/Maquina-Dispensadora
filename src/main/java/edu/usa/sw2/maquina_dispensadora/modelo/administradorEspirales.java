a0bdf7279d009073dbcdcded8f8d611178eaa006
LAPTOP-LLS35HD6\paola <paolavargas0115@gmail.com>
2017-05-21 15:13:19 -0500

    avence administradorEspirales

--- src/main/java/edu/usa/sw2/maquina_dispensadora/modelo/administradorEspirales.java
+++ src/main/java/edu/usa/sw2/maquina_dispensadora/modelo/administradorEspirales.java
@@ -1,5 +1,19 @@
 package edu.usa.sw2.maquina_dispensadora.modelo;
 
-public class administradorEspirales {
+public class administradorEspirales  {
 
+	public Espiral espiral; 
+	
+	
+	public administradorEspirales() {
+	    espiral= new Espiral();
+	}
+	public  String conseguirEspiral (String codigoIngresado){
+		if(codigoIngresado==espiral.codigo){
+		return codigoIngresado;
+		}
+		return "codigo erroneo de espiral";
+	}
+
+	
 }

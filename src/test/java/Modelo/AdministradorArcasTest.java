acc42cbc8823f58d6265b38f75d5a7dab98e0896
LAPTOP-LLS35HD6\paola <paolavargas0115@gmail.com>
2017-05-25 16:22:35 -0500

    test

--- src/test/java/Modelo/AdministradorArcasTest.java
+++ src/test/java/Modelo/AdministradorArcasTest.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
 
-<<<<<<< HEAD
+
 public class AdministradorArcasTest {
 
 	AdministradorArcas administradorArcas;
@@ -19,21 +19,8 @@ public class AdministradorArcasTest {
 	@Test
 	public void deberiaDarCadaArca() throws IOException {
 		assertEquals("5,1000,14,50",administradorArcas.obtenerArca(1000));
-=======
 
-public class AdministradorArcasTest {
+	}
 
-	AdministradorArcas administradorArcas;
-	
-	@Before
-	public void SetUp(){
-		administradorArcas = new AdministradorArcas();
-	}
-	
-	@Test
-	public void algo() throws IOException {
-		assertEquals("",administradorArcas.obtenerArca(0));
->>>>>>> 24486fe3adfb04d45c59ba99ea170e44f36fa4f7
-	}
 
 }

7aab44e43c90131b28fc4e810aee6c67ed93b528
Miguel Alejandro Rodriguez Sierra <miguelal.rodriguez@correo.usa.edu.co>
2017-05-25 14:41:32 -0500

    Controlador arcas pruebas

--- src/test/java/Controlador/ControladorArcasTest.java
+++ src/test/java/Controlador/ControladorArcasTest.java
@@ -1,28 +1,15 @@
 package Controlador;
 
-import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
 
 public class ControladorArcasTest {
 	
 	ControladorArcas controladorDeArcas;
-	int entradaSaldo;
 	
 	@Before
 	public void SetUp(){
 		controladorDeArcas = new ControladorArcas();
 	}
 	
-	@Test
-	public void debeDarErrorSiEntrada5(){
-		assertEquals("", 5);
-	}
-	
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 
 }
